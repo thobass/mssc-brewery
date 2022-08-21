ab7d575fb90b1fc9a7225091fbd2afca0d3ab40b
BASSET, Thomas <thomas.basset@cgi.com>
2022-08-21 11:56:45 +0200

    Add global error handling

--- src/main/java/rocks/basset/msscbrewery/web/controller/BeerController.java
+++ src/main/java/rocks/basset/msscbrewery/web/controller/BeerController.java
@@ -54,15 +54,4 @@ public class BeerController {
     public void handleDelete(@PathVariable("beerId") UUID beerId){
         beerService.deleteById(beerId);
     }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e){
-        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
-
-        e.getConstraintViolations().forEach(constraintViolation -> {
-            errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage());
-        });
-
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
 }
--- src/main/java/rocks/basset/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/rocks/basset/msscbrewery/web/controller/CustomerController.java
@@ -53,15 +53,4 @@ public class CustomerController {
     public void handleDelete(@PathVariable("customerId") UUID customerId){
         customerService.deleteCustomerById(customerId);
     }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e){
-        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
-
-        e.getConstraintViolations().forEach(constraintViolation -> {
-            errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage());
-        });
-
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
 }
