e97c697af784f8a8c62fd4965864adf26f5a56fd
BASSET, Thomas <thomas.basset@cgi.com>
2022-08-22 18:29:49 +0200

    Working with custom serializer

--- src/main/java/rocks/basset/msscbrewery/web/model/BeerDto.java
+++ src/main/java/rocks/basset/msscbrewery/web/model/BeerDto.java
@@ -2,6 +2,7 @@ package rocks.basset.msscbrewery.web.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -39,4 +41,7 @@ public class BeerDto {
     @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createDate;
     private OffsetDateTime lastUpdateDate;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate myLocalDate;
 }
--- src/test/java/rocks/basset/msscbrewery/web/model/BaseTest.java
+++ src/test/java/rocks/basset/msscbrewery/web/model/BaseTest.java
@@ -4,10 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
-public class BaseTest {
+class BaseTest {
 
     @Autowired
     ObjectMapper objectMapper;
@@ -21,6 +22,7 @@ public class BaseTest {
                 .lastUpdateDate(OffsetDateTime.now())
                 .price(new BigDecimal("12.99"))
                 .upc(123123123123L)
+                .myLocalDate(LocalDate.now())
                 .build();
     }
 }
